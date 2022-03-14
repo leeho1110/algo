d8b346e50989822a42b222403333c90046523913
holee <ho.sol.lee@gmail.com>
2022-03-14 22:42:16 +0900

    test: 키패드 누르기 테스트 코드 추가

--- src/test/java/ps/PressKeypadTest.java
+++ src/test/java/ps/PressKeypadTest.java
@@ -17,15 +17,20 @@ public class PressKeypadTest {
 
     @DisplayName("통합 테스트")
     @Test
-    public void pressKeyPadIntergrationTest(){
+    public void pressKeyPadIntegrationTest(){
 
         String testCase1 = pressKeypad.solution(new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5}, "right");
         String testCase2 = pressKeypad.solution(new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}, "left");
         String testCase3 = pressKeypad.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, "right");
 
-
         assertEquals(testCase1, "LRLLLRLLRRL");
-        assertEquals(testCase2, "LRLLLRLLRRL");
-        assertEquals(testCase3, "LRLLLRLLRRL");
+        assertEquals(testCase2, "LRLLRRLLLRR");
+        assertEquals(testCase3, "LLRLLRLLRL");
+    }
+
+    @DisplayName("주사용손이 제대로 파싱되는가")
+    @Test
+    public void initHand(){
+        String hand = "right";
     }
 }
