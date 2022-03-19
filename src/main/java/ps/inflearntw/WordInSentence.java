5fd087c81d1fdb463d2f01693531ea7a8e74f950
holee <ho.sol.lee@gmail.com>
2022-03-20 00:31:43 +0900

    feat: 문장 속 단어
    https://cote.inflearn.com/contest/10/problem/01-03

---
+++ src/main/java/ps/inflearntw/WordInSentence.java
@@ -0,0 +1,36 @@
+package ps.inflearntw;
+
+import java.util.Scanner;
+
+public class WordInSentence {
+
+    private String answer = "";
+
+    public String solution(String inputString){
+        String answer = "";
+        String[] wordSplitedBySpace = inputString.split(" ");
+
+        for(int i=wordSplitedBySpace.length-1; i>=0; i--){
+            String word = wordSplitedBySpace[i];
+            if(isLongerThan(word)){
+                answer = word;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isLongerThan(String word){
+        return answer.length() <= word.length();
+    }
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+
+        String inputString = in.nextLine();
+
+        WordInSentence m = new WordInSentence();
+        System.out.println(m.solution(inputString));
+
+    }
+}
