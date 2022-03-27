085ada93b719d8d4d879e8e0dd5b696ad56ac95b
holee <ho.sol.lee@gmail.com>
2022-03-27 22:41:17 +0900

    feat: 중복 문자 제거

---
+++ src/main/java/ps/inflearntw/DeleteDuplicateAlphabet.java
@@ -0,0 +1,28 @@
+package ps.inflearntw;
+
+import java.util.*;
+
+public class DeleteDuplicateAlphabet {
+
+    private Set<String> findedAlphabet = new LinkedHashSet<>();
+
+    public String solution(String input){
+
+        for(char alphabet : input.toCharArray()){
+            findedAlphabet.add(String.valueOf(alphabet));
+        }
+
+        StringBuilder answer = new StringBuilder();
+        Arrays.stream(findedAlphabet.toArray()).forEach(a -> answer.append(a));
+
+        return answer.toString();
+    }
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String input = in.next();
+
+        DeleteDuplicateAlphabet m = new DeleteDuplicateAlphabet();
+        System.out.println(m.solution(input));
+    }
+}
