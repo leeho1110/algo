46aabbc933d088127a2679e6719a1b259992dd61
holee <ho.sol.lee@gmail.com>
2022-03-30 22:48:02 +0900

    feat: 숫자만 추출

---
+++ src/main/java/ps/inflearntw/ExtractNumber.java
@@ -0,0 +1,29 @@
+package ps.inflearntw;
+
+import java.util.Scanner;
+
+public class ExtractNumber {
+
+    public int solution(String input){
+        String numberString = "";
+
+        String onlyNum = extractNumber(input);
+        for(char each : onlyNum.toCharArray()){
+            numberString += each;
+        }
+
+        return Integer.parseInt(numberString);
+    }
+
+    private String extractNumber(String input){
+        return input.replaceAll("[^0-9]","");
+    }
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String input = in.next();
+
+        ExtractNumber main = new ExtractNumber();
+        System.out.println(main.solution(input));
+    }
+}
