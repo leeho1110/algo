81b075183a1d0cb98c9badde4d5a44c7370af9ac
holee <ho.sol.lee@gmail.com>
2022-04-23 22:33:25 +0900

    test: 가장 짧은 문자 거리
    테스트 케이스 추가

--- src/test/java/ps/inflearntw/ShortestDistanceTest.java
+++ src/test/java/ps/inflearntw/ShortestDistanceTest.java
@@ -1,6 +1,5 @@
 package ps.inflearntw;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,15 +8,30 @@ public class ShortestDistanceTest {
 
     private static ShortestDistanceNew shortestDistance;
 
-    @BeforeAll
-    public static void setUp(){
+    @Test
+    public void integration1(){
         String[] inputList = "teachermode e".split(" ");
         shortestDistance = new ShortestDistanceNew(String.valueOf(inputList[1].charAt(0)), inputList[0]);
+
+        int[] answer = {1, 0, 1, 2, 1, 0, 1, 2, 2, 1, 0};
+        assertThat(shortestDistance.checkDistance()).isEqualTo(answer);
     }
 
     @Test
-    public void integration(){
-        int[] answer = {1, 0, 1, 2, 1, 0, 1, 2, 2, 1, 0,};
+    public void integration2(){
+        String[] inputList = "fkdgkjdflkgjljslgjkfldjlkfdg f".split(" ");
+        shortestDistance = new ShortestDistanceNew(String.valueOf(inputList[1].charAt(0)), inputList[0]);
+
+        int[] answer = {0, 1, 2, 3, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3 ,2 ,1 ,0 ,1, 2, 3, 2, 1, 0, 1, 2};
+        assertThat(shortestDistance.checkDistance()).isEqualTo(answer);
+    }
+
+    @Test
+    public void integration3(){
+        String[] inputList = "eochjgoekghlakegh h".split(" ");
+        shortestDistance = new ShortestDistanceNew(String.valueOf(inputList[1].charAt(0)), inputList[0]);
+
+        int[] answer = {3, 2, 1, 0, 1, 2, 3, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0};
         assertThat(shortestDistance.checkDistance()).isEqualTo(answer);
     }
 }
