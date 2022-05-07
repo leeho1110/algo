697ff02a4ea7dcfd6f4432b637c1699c58099428
holee <ho.sol.lee@gmail.com>
2022-05-07 13:41:05 +0900

    feat: 보이는 학생

---
+++ src/main/java/ps/inflearntw/VisibleStudent.java
@@ -0,0 +1,35 @@
+package ps.inflearntw;
+
+public class VisibleStudent {
+
+    private int criteria;
+    private final int[] heights;
+    private int answer = 1;
+
+    public VisibleStudent(int[] heights) {
+        this.heights = heights;
+        this.criteria = heights[0];
+    }
+
+    public boolean isOverThanCriteria(int i) {
+        return this.criteria < heights[i];
+    }
+
+    public int findVisibleStudentsCount() {
+        for(int i=1; i<heights.length; i++){
+            if(isOverThanCriteria(i)){
+                addVisibleCount();
+                changeCriteria(i);
+            }
+        }
+        return answer;
+    }
+
+    private void changeCriteria(int index) {
+        this.criteria = heights[index];
+    }
+
+    private void addVisibleCount(){
+        this.answer++;
+    };
+}
