9a5986b3fc935b2b8192ce54d868628da9867be1
holee <ho.sol.lee@gmail.com>
2022-07-02 20:28:19 +0900

    refactor: 괄호 문자 제거

--- src/main/java/ps/inflearntw/stackandqueue/RemoveTextInBracket.java
+++ src/main/java/ps/inflearntw/stackandqueue/RemoveTextInBracket.java
@@ -1,16 +1,12 @@
 package ps.inflearntw.stackandqueue;
 
-import java.awt.*;
-import java.time.temporal.ChronoField;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Stack;
 
 public class RemoveTextInBracket {
-
-
     private final String text;
     private StringBuilder answer = new StringBuilder();
+    private Stack<Character> leftBracketStack = new Stack<>();
+    private Stack<Character> rightBracketStack = new Stack<>();
 
     public RemoveTextInBracket(String text) {
         this.text = text;
@@ -20,21 +16,35 @@ public class RemoveTextInBracket {
     //  #1. 문자열을 돌며 '('가 나오면 스택에 넣기
     //  #2. 스택에 있는 '('의 크기가 문자열을 돌며 나온 ')'의 크기가 같아질 때까지 answer에 넣지말기
     public String extractNotInBracket() {
-        Stack<Character> leftBracketStack = new Stack<>();
-        Stack<Character> rightBracketStack = new Stack<>();
-
         for (Character c : text.toCharArray()) {
-            if(c == '('){
-                leftBracketStack.push(c);
-            } else if(c == ')'){
-                rightBracketStack.push(c);
-            } else {
-                if(leftBracketStack.size() == rightBracketStack.size()){
-                    answer.append(c);
-                }
+            findCharacterNotInBracket(c);
+        }
+        return answer.toString();
+    }
+
+    private void findCharacterNotInBracket(Character c) {
+        if(isBracket(c)){
+            pushToBracketStack(c);
+        } else {
+            if(hasBracketStackSameSize()){
+                answer.append(c);
             }
         }
+    }
 
-        return answer.toString();
+    private boolean hasBracketStackSameSize() {
+        return leftBracketStack.size() == rightBracketStack.size();
+    }
+
+    private void pushToBracketStack(Character c) {
+        if (c == '(') {
+            leftBracketStack.push(c);
+        } else {
+            rightBracketStack.push(c);
+        }
+    }
+
+    private boolean isBracket(Character c) {
+        return c == '(' || c == ')';
     }
 }
