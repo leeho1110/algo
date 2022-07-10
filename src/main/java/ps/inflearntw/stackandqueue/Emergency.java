d5b2d19af2ce5e0242e5bad415f2a981cdf9fb44
holee <ho.sol.lee@gmail.com>
2022-07-10 23:52:41 +0900

    refactor: 응급실

--- src/main/java/ps/inflearntw/stackandqueue/Emergency.java
+++ src/main/java/ps/inflearntw/stackandqueue/Emergency.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Emergency {
     private final Queue<Patient> patients = new ArrayDeque<>();
     private Integer answer = 0;
+    private int sequence;
 
     public Emergency(int[] patients) {
         for (int i = 0; i < patients.length; i++) {
@@ -13,25 +14,38 @@ public class Emergency {
     }
 
     public Integer takeCare(int sequence) {
-       while(!patients.isEmpty()){
-            Patient patient = patients.poll();
+        this.sequence = sequence;
+        while (!patients.isEmpty()) {
+            Patient target = patients.poll();
 
-            for (Patient patient1 : patients) {
-                if (patient1.getPriority() > patient.getPriority()) {
-                    patients.offer(patient);
-                    patient = null;
+            for (Patient patient : patients) {
+                if (isMoreDangerousThan(patient, target)) {
+                    patients.offer(target);
+                    target = null;
                     break;
                 }
             }
 
-            if (patient != null) {
+            if (isMostDangerous(target)) {
                 answer++;
-                if (patient.getSequence() == sequence) {
+                if (isEqualToSequence(target)) {
                     return answer;
                 }
             }
         }
         return answer;
+    }
+
+    private boolean isEqualToSequence(Patient target) {
+        return target.getSequence() == sequence;
+    }
+
+    private boolean isMostDangerous(Patient target) {
+        return target != null;
+    }
+
+    private boolean isMoreDangerousThan(Patient patient, Patient target) {
+        return patient.getPriority() > target.getPriority();
     }
 
     static class Patient {
