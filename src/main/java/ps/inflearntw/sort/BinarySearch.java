b684ce379981ee05414bdd958331ebbf758fab75
holee <ho.sol.lee@gmail.com>
2022-07-18 19:43:30 +0900

    refactor: 이분검색

--- src/main/java/ps/inflearntw/sort/BinarySearch.java
+++ src/main/java/ps/inflearntw/sort/BinarySearch.java
@@ -2,12 +2,31 @@ package ps.inflearntw.sort;
 
 public class BinarySearch {
     private final int[] numbers;
+    private int answer;
 
     public BinarySearch(int[] numbers) {
         this.numbers = numbers;
     }
 
     public int find(int target) {
+        bubbleSort();
+        findIndexOf(target);
+        return answer;
+    }
+
+    private void findIndexOf(int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            if(isEqualToTarget(target, i)){
+                answer = i+1;
+            }
+        }
+    }
+
+    private boolean isEqualToTarget(int target, int i) {
+        return numbers[i] == target;
+    }
+
+    private void bubbleSort() {
         for(int i=0; i<numbers.length; i++){
             for(int j=0; j< numbers.length-1-i; j++){
                 if(numbers[j] > numbers[j+1]){
@@ -17,14 +36,5 @@ public class BinarySearch {
                 }
             }
         }
-
-        int answer = -1;
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] == target){
-                answer = i+1;
-            }
-        }
-
-        return answer;
     }
 }
