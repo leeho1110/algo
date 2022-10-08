4f53ad639b7fcc13ae052623fac29d071e9c4b09
holee <ho.sol.lee@gmail.com>
2022-10-09 00:51:09 +0900

    feat: 삽입 정렬 재구현

--- src/main/java/ps/inflearntw/sort/InsertSort.java
+++ src/main/java/ps/inflearntw/sort/InsertSort.java
@@ -1,5 +1,10 @@
 package ps.inflearntw.sort;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class InsertSort {
     private final int[] numbers;
 
@@ -8,27 +13,43 @@ public class InsertSort {
     }
 
     public int[] sort() {
+        // 삽입 정렬은
+        // 전체 배열에서 1번 인덱스에서 시작하며 하나씩 내려가면 자신 뒤에 있는 숫자와 비교해 값을 바꾼다
 
-
-        // 삽입 정렬은 순회탐색하며
-        for(int i=1; i<numbers.length; i++){
-            int target = numbers[i];
-            int swapIdx = -1;
-            // 자신보다 뒤에 있는 요소들을 비교한다
-            for(int j=i-1; j>=0; j--){
-                // 한칸씩 내려가면서 만약 자신보다 크다면
-                if(target < numbers[j]){
-                    // 자신 바로 뒤로 밀어버리는 방식으로 정렬한다
-                    numbers[j+1] = numbers[j];
-                    swapIdx = j;
-                } else {
-                    // 크지 않다면 더이상 큰 요소가 없는 것이므로 탐색을 종료한다
-                    break;
+        // 전체 배열에서 두번쨰 인덱스부터 탐색을 시작한다
+        for (int i = 1; i < numbers.length; i++) {
+            // 자신의 한칸 뒤로 움직이며 자신보다 크면 뒤로, 작으면 그대로 둔다
+            for (int j = i; j > 0 ; j--) {
+                if(numbers[j-1] > numbers[j]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = temp;
                 }
             }
-            numbers[swapIdx] = target;
         }
 
         return numbers;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = new int[Integer.parseInt(bufferedReader.readLine())];
+
+        String numbersString = bufferedReader.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(numbersString," ");
+
+        int idx=0;
+        while (tokenizer.hasMoreTokens()){
+            numbers[idx++] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        InsertSort sort = new InsertSort(numbers);
+        int[] sort1 = sort.sort();
+
+        for (int i : sort1) {
+            System.out.print(i + " ");
+        }
+
     }
 }
