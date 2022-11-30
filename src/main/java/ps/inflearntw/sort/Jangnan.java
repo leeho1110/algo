6a2bb82f79d8f70b9a150b665141e195f25b0a97
holee <ho.sol.lee@gmail.com>
2022-12-01 00:14:57 +0900

    refactor: 장난꾸러기 O(n^2) -> O(n)
    대신 공간 복잡도가 n만큼 증가

--- src/main/java/ps/inflearntw/sort/Jangnan.java
+++ src/main/java/ps/inflearntw/sort/Jangnan.java
@@ -1,9 +1,6 @@
 package ps.inflearntw.sort;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Jangnan {
@@ -16,20 +13,11 @@ public class Jangnan {
     public int[] find() {
         List<Integer> answer = new ArrayList<>();
 
-        int[] rightOrder = students.clone();
+        int[] sortedStudents = students.clone();
+        Arrays.sort(sortedStudents);
 
-        for(int i=0; i< rightOrder.length; i++){
-            for(int j=0; j<rightOrder.length-i-1; j++){
-                if(rightOrder[j] > rightOrder[j+1]){
-                    int temp = rightOrder[j+1];
-                    rightOrder[j+1] = rightOrder[j];
-                    rightOrder[j] = temp;
-                }
-            }
-        }
-
-        for(int i=0; i< students.length; i++){
-            if(students[i] != rightOrder[i]){
+        for (int i = 0; i <students.length; i++) {
+            if(students[i] != sortedStudents[i]){
                 answer.add(i+1);
             }
         }
