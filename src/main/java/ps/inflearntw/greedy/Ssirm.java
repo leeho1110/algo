1087ae6e9261f672a50d253e13e4dd353d4c3961
holee <ho.sol.lee@gmail.com>
2022-12-03 19:49:30 +0900

    feat: 씨름 선수 (Greedy, O(N))

--- src/main/java/ps/inflearntw/greedy/Ssirm.java
+++ src/main/java/ps/inflearntw/greedy/Ssirm.java
@@ -1,6 +1,8 @@
 package ps.inflearntw.greedy;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Ssirm {
 
@@ -18,32 +20,42 @@ public class Ssirm {
      * 첫째 줄에 바둑 선수로 뽑히는 최대 인원을 출력하세요.
      */
 
+    static class Body implements Comparable<Body> {
+        private int height;
+        private int weight;
+
+        public Body(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Body another) {
+            return another.height - this.height;
+        }
+    }
     private int answer;
 
-    public int findMax(int[][] members) {
+    public int findMax(List<Body> members) {
+        /**
+         * Collections.sort를 통해 가장 키가 큰 사람보다 나머지 모든 사람이 키가 작다는 것을 가정
+         * 만약 선발되기 위해선 키, 몸무게 중 하나라도 높아야 한다.
+         *
+         * 순회 탐색하며 현재까지 발견한 몸무게의 최대값보다 높은지 확인한다.
+         * 그렇다면 키는 작아도 몸무게가 높으므로 선발된다.
+         *
+         * 그리고 선발 가능 여부를 판단하는 최대 몸무게는 갱신되어야 한다.
+         * 만약 그렇지 않다면 키(자동 정렬로 인해 당연히 작고)와 몸무게(기준이 변경되지 않아)가 모두 작은 누군가가 선발되므로
+         */
 
-        m
+        Collections.sort(members);
+        int maxWeight = Integer.MIN_VALUE;
 
-        // 전체 순회 탐색
-        for (int i = 0; i < members.length; i++) {
-            boolean choiceFlag = true;
-
-            for (int j = 0; j < members.length; j++) {
-
-                if(members[j][0] == members[i][0] && members[j][1] == members[i][1]){
-                    continue;
-                }
-
-                if (members[j][0] > members[i][0] && members[j][1] > members[i][1]) {
-                    choiceFlag = false;
-                    break;
-                }
-            }
-
-            if(choiceFlag){
+        for (Body member : members) {
+            if(member.weight > maxWeight){
                 answer++;
+                maxWeight = member.weight;
             }
-
         }
 
         return answer;
--- src/test/java/ps/inflearntw/greedy/SsirmTest.java
+++ src/test/java/ps/inflearntw/greedy/SsirmTest.java
@@ -2,6 +2,8 @@ package ps.inflearntw.greedy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,11 +12,16 @@ class SsirmTest {
     @Test
     void findMax() {
         // given:
-        int[][] members = {{172,67},{183,65},{180,70},{170,72},{181,60}};
         Ssirm sut = new Ssirm();
 
         // when:
-        int result = sut.findMax(members);
+        int result = sut.findMax(Arrays.asList(
+            new Ssirm.Body(172,67),
+            new Ssirm.Body(183,65),
+            new Ssirm.Body(180,70),
+            new Ssirm.Body(170,72),
+            new Ssirm.Body(181,60)
+        ));
 
         // then:
         assertThat(result).isEqualTo(3);
