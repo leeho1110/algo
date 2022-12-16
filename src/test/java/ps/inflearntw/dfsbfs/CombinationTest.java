97d9e9c063c9604f351ad42afa73e826cf642f2a
holee <ho.sol.lee@gmail.com>
2022-12-16 23:49:00 +0900

    test: 조합수 구하기 (DFS, 메모이제이션 사용해서)

--- src/test/java/ps/inflearntw/dfsbfs/CombinationTest.java
+++ src/test/java/ps/inflearntw/dfsbfs/CombinationTest.java
@@ -5,7 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CombinationTest {
-    
+
+    @Test
+    public void test(){
+        // given
+        Combination sut = new Combination();
+
+        // when
+        int result = sut.findWithMemoization(33, 19);
+
+        // then
+        assertEquals(result,818809200); // 99ms
+    }
+
     @Test
     public void testNoMemoization(){
         // given
