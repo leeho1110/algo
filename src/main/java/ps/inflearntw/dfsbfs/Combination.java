519be94ea21b53957a69cc2706435dffa5bfad36
holee <ho.sol.lee@gmail.com>
2022-12-16 23:48:15 +0900

    feat: 조합수 구하기 (DFS, 메모이제이션 사용해서)

--- src/main/java/ps/inflearntw/dfsbfs/Combination.java
+++ src/main/java/ps/inflearntw/dfsbfs/Combination.java
@@ -2,6 +2,25 @@ package ps.inflearntw.dfsbfs;
 
 public class Combination {
 
+    private int[][] memo = new int[0][0];
+
+    public int findWithMemoization(int entire, int select) {
+        this.memo = new int[entire + 1][entire + 1];
+        return combination(entire, select);
+    }
+
+    public int combination(int entire, int select) {
+        if (memo[entire][select] > 0) {
+            return memo[entire][select];
+        }
+
+        if (entire == select || select == 0) {
+            return 1;
+        } else {
+            return memo[entire][select] = combination(entire - 1, select - 1) + combination(entire - 1, select);
+        }
+    }
+
     public int combinationNoMemo(int entire, int select) {
         if (entire == select || select == 0) {
             return 1;
