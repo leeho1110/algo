31ad895cc38b8344a51a9d95dcc42f3505c2c2a5
holee <ho.sol.lee@gmail.com>
2023-01-10 01:02:40 +0900

    test: 회의실 배정 (Greedy)

---
+++ src/test/java/ps/inflearntw/greedy/ConferenceTest.java
@@ -0,0 +1,29 @@
+package ps.inflearntw.greedy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConferenceTest {
+
+    @Test
+    public void test(){
+        // given
+        Conference sut = new Conference();
+
+        // when
+        int result = sut.findMax(Arrays.asList(
+                new Conference.ConferenceTime(1,4),
+                new Conference.ConferenceTime(2,3),
+                new Conference.ConferenceTime(3,5),
+                new Conference.ConferenceTime(4,6),
+                new Conference.ConferenceTime(5,7)
+        ));
+
+        // then
+        assertThat(result).isEqualTo(3);
+    }
+
+}
